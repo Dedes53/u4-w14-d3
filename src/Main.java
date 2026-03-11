1ea03c13edf97d9e477c4dd7d9ca83790e528ab0
Dedes <federico.lepore73@gmail.com>
2026-03-11 17:30:27 +0100

    save

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,15 @@ public class Main {
 
         Rettangolo rettangolo = new Rettangolo(3.7, 2.5);
         Rettangolo rettangolo2 = new Rettangolo(6.9, 4.4);
-
 //        System.out.println(rettangolo.perimetro());
 //        System.out.println(rettangolo2.perimetro());
 //        System.out.println(rettangolo.area());
 //        System.out.println(rettangolo2.area());
         System.out.println(stampa2Rettangoli(rettangolo, rettangolo2));
 
-
         Sim federico = new Sim(3394667372L);
         federico.stampaSim();
+
 
     }
 }
